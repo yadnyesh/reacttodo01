2fab3311b82d2c341f7ca10b566796933ccadddd
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-10-17 07:08:33 +0530

    create constructor and inject todo array

--- src/App.js
+++ src/App.js
@@ -24,6 +24,14 @@ var todos = [
 ];
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos
+    }
+  }
+
   render() {
     return (
       <div className="App">
